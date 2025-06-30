c58ef81376bfdc8ea6d34dbf96f2565494f0877c
Allen Chung <allenchung75@gmail.com>
2025-06-30 16:38:32 -0400

    Initial commit

---
+++ src/test/java/com/example/budgetcalculator/BudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budgetcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
